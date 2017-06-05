eddc35e82a8d62684ebab9257102d644228bd393
sagar chopada <chopada.sagar32@gmail.com>
2017-06-06 00:05:10 +0530

    djkjkkjjsad

--- index.js
+++ index.js
@@ -10,7 +10,105 @@ app.use(bodyParser.json());
 app.get('/', function (req, res) {
     res.send("Hello Sagar...!");
 });
+app.post("/talent_employer", function (req, res) {
+    mySession = req.session;
+    sid = req.sessionID;
+    var result = req.body.result;
+    var speech = "Seems like some problem. Speak again.";
+    var messages = [];
+    if (result.contexts[0].name == 'usertype' && result.action == "i_am") {
+        mySession.userType = result.contexts[0].parameters.userType;
+        speech = "Okay you are " + mySession.userType + ". What you want to know about glasssquid.io?";
+        return res.json({
+            speech: speech,
+            displayText: speech,
+            source: 'glasssquid_faq'
+        });
+    }
 
+
+    try {
+        /**
+         * if userType not available
+         */
+        if (!mySession.userType) {
+            speech = "are you employer or talent?";
+
+        }
+
+        /**
+        * if user type Talent
+        */
+        if (mySession.userType == 'Talent') {
+            speech = "talent bot call";
+            var text = result.resolvedQuery;
+            request({
+                url: "https://api.api.ai/v1/query?v=20150910",
+                method: 'POST',
+                json: {
+                    query: text,
+                    lang: "en",
+                    sessionId: "abcdefghijklmn123456789"
+                },
+                headers: { "Authorization": "Bearer c594a86ef69346ddb7e410631f9603d7" }
+            }, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body)
+                    if (body && body.result && body.result.fulfillment) {
+                        return res.json(body.result.fulfillment);
+                    }
+                }
+            });
+
+        }
+        /**
+         * else if user Employer
+         */
+        else if (mySession.userType == 'Employer') {
+            speech = "employer bot call";
+            var text = result.resolvedQuery;
+            request({
+                url: "https://api.api.ai/v1/query?v=20150910",
+                method: 'POST',
+                json: {
+                    query: text,
+                    lang: "en",
+                    sessionId: "abcdefghijklmn123456789"
+                },
+                headers: { "Authorization": "Bearer 373788d7794e4493bb15560d19efda3f" }
+            }, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body)
+                    if (body && body.result && body.result.fulfillment) {
+                        return res.json(body.result.fulfillment);
+                    }
+
+                }
+            });
+
+        }
+    }
+    catch (err) {
+        speech = "Seems like some problem. Speak again.";
+    }
+    finally {
+        if (messages.length > 0) {
+            return res.json({
+                speech: messages[0].speech,
+                messages: messages,
+                source: 'glasssquid_faq'
+            });
+        } else {
+            return res.json({
+                speech: speech,
+                displayText: speech,
+                source: 'glasssquid_faq'
+            });
+        }
+    }
+
+
+});
 app.post("/echo", function (req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
